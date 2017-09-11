56d04f7b547f656cff693c5c32479f423beb8ad7
Gregory Rudolph <rudi@lolay.com>
2017-09-11 09:25:13 -0400

    Initial Commit

---
+++ Timeline/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Timeline
+//
+//  Created by Gregory Rudolph-Alverson on 9/11/17.
+//  Copyright © 2017 CS350. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
