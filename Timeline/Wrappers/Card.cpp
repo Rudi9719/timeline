504e193f61ef1def1e855db1c71171b4e2653c38
Rudi <gregory@STEiNBookPro.local>
2017-09-27 10:08:12 -0400

    Thanks, High Sierra.

--- Timeline/Wrappers/Card.cpp
+++ Timeline/Wrappers/Card.cpp
@@ -7,3 +7,50 @@
 //
 
 #include "Card.hpp"
+
+Card::Card()
+{
+    this -> cardtype = -1;
+    column = -1;
+    y = -1;
+    for (int i = 0; i > this -> paths.size();i++) {
+       this -> paths.insert(-1); // give paths placer values
+    }
+    for (int i = 0; i > this -> comtaken.size();i++) {
+        this -> comtaken.insert(false); // gives commodity bools placer values
+    }
+    for (int i = 0; i > this -> comtype.size();i++) {
+        this -> comtype.insert(-1); // gives comtypes placer values
+    }
+}
+int getCardType()
+{
+    return cardtype;
+}
+void setCardType(int _cardtype)
+{
+    this -> cardtype = _cardtype;
+}
+void setY(int _y) {
+    y = _y;
+}
+vector<int> getPaths() {
+    return paths;
+}
+void setPaths(vector<int> _v) {
+    // Totally wipes current object's path tree
+    this.paths.erase(this.paths.begin(),this.paths.end());
+    // Replaces it with the values of the passed vector
+    for( int i = 0;i<this.paths.size(); i++) {
+        this.paths.insert(_v[i]); // the magic line...
+    }
+}
+int getColumn() {
+    return this.column;
+}
+vector<int> getComType() {
+    return comtype;
+}
+vector<bool> getComTaken() {
+    return comtaken;
+}
--- Timeline/Wrappers/Card.hpp
+++ Timeline/Wrappers/Card.hpp
@@ -9,12 +9,29 @@
 #ifndef Card_hpp
 #define Card_hpp
 
-#include <stdio.h>
-class Card {
+#import <vector>
+
+
+class Card
+{
+private:
+    int cardtype;
+    int column;
+    int y;
+    std::vector<int> paths[4];
+    std::vector<bool> comtaken[4];
+    std::vector<int> comtype[4];
+    
 public:
     Card();
-    void setImage(const char* path);
-    void setPosition(int x, int y);
+    int getCardType();
+    void setCardType(int);
+    int getY();
+    void setY(int);
+    std::vector<int> getPaths();
+    void setPaths(std::vector<int>);
+    int getColumn();
+    std::vector<int> getComType();
+    std::vector<bool> getComTaken();
 };
-
 #endif /* Card_hpp */
