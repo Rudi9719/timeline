b0a83a04d8b9e0fb0bbbc31fbdccaa73b51ef091
Taylor Hines <thines@oogieboogie.local>
2017-10-09 19:07:13 -0400

    Added Deck classes

---
+++ Timeline/Classes/Deck.hpp
@@ -0,0 +1,30 @@
+//
+//  Deck.hpp
+//  Timeline
+//
+//  Created by Rudi on 10/4/17.
+//  Copyright © 2017 STEiN-Net. All rights reserved.
+//
+
+#ifndef Deck_hpp
+#define Deck_hpp
+
+#include <stdio.h>
+#include <vector>
+#include "SDL_Wrapper.hpp"
+
+class Deck {
+public:
+    Deck(std::vector<Card> cards);
+    std::vector<Card>* deck;
+    Card* drawCard();
+    
+    
+private:
+    std::vector<Card> unshuffled;
+    std::vector<Card> shuffled;
+    
+};
+
+
+#endif /* Deck_hpp */
