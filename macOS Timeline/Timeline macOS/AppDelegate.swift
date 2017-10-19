3c8fae472da59a268a45b500d2c7eb5bb03a653c
Rudi <gregory@stein13.net>
2017-10-19 10:38:29 -0400

    Added all macOS files

---
+++ macOS Timeline/Timeline macOS/AppDelegate.swift
@@ -0,0 +1,30 @@
+//
+//  AppDelegate.swift
+//  Timeline macOS
+//
+//  Created by Rudi on 10/15/17.
+//  Copyright © 2017 Rudi. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+    func applicationShouldTerminateAfterLastWindowClosed(_ sender: NSApplication) -> Bool {
+        return true
+    }
+
+
+}
+
