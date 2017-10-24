b90a9c77e11c1cbc940532def5a408efe8ee7c3a
Taylor Hines <31854883+tayhines12@users.noreply.github.com>
2017-10-24 15:28:57 -0400

    Majority of file written

--- Timeline/Tests/Unit/CardUnitTest.cpp
+++ Timeline/Tests/Unit/CardUnitTest.cpp
@@ -2,8 +2,53 @@
 //  CardUnitTest.cpp
 //  Timeline
 //
-//  Created by Rudi on 10/19/17.
-//  Copyright © 2017 STEiN-Net. All rights reserved.
+//  Created by iduR on 6/6/6.
+//  Copyright © 2017 iduR-Net. All rights reserved.
 //
 
 #include <stdio.h>
+#include "Card_hpp"
+#include "UIButton.hpp"
+
+SDL_Rect rect;
+
+Card myCard(rect);
+
+//getCardTypeTest
+cout << myCard.getCardType() << endl;
+
+//setCardTypeTest
+myCard.setCardType(1);
+cout << myCard.getCardType() << endl;
+
+//moveCardTest
+myCard.moveCard(100,100);
+
+//getPathsTests
+int* p;
+p = myCard.getPaths();
+cout << p << endl;
+
+//handleClickTest
+myCard.handleClick(3,4);
+
+
+//getColumnTest
+int col;
+col = getColumn();
+cout << col << endl;
+
+//getComTypeTest
+int* point;
+point = getComType();
+cout << point << endl;
+
+//getComTakenTest
+int* pointer;
+pointer = getComTaken();
+cout << pointer << endl;
+
+
+
+
+
