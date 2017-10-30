d2b30621deb25ff03799c23c0a7ec4e27bda0c0b
Taylor Hines <thines@skellington.local>
2017-10-30 11:27:24 -0400

    Force Merge taylor/deck into master

--- Timeline/Classes/Deck.cpp
+++ Timeline/Classes/Deck.cpp
@@ -6,6 +6,24 @@
 //  Copyright © 2017 STEiN-Net. All rights reserved.
 //
 
+
 #include "Deck.hpp"
+#include <vector>
 
-
+Deck::Deck(std::vector<Card> cards) {
+    this -> unshuffled = cards;
+    for(int i = 0; i < 46; i++ ) {
+        int random = 0; // fix this
+        this -> shuffled.push_back(cards.at(random)); // push back card at random
+        // You need to iterate over the unshuffled
+        // Look into vector for loop iterator
+        // that way you can use erase (iterator)
+        cards.erase(<#const_iterator __position#>); // See?
+    }
+    
+}
+Card* Deck::drawCard() {
+    // Return address of card & remove it
+    
+    return nullptr; // Haha no.
+}
