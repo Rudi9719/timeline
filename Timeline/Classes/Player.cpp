2d90f97f560a1558b1674824f83de43f4fea92c0
Gregory Rudolph <rudi@nightmare.haus>
2017-11-29 14:46:59 -0500

    Class creation

---
+++ Timeline/Classes/Player.cpp
@@ -0,0 +1,11 @@
+//
+//  Player.cpp
+//  Timeline
+//
+//  Created by Gregory Rudolph on 11/29/17.
+//  Copyright © 2017 Gregory Rudolph. All rights reserved.
+//
+
+#include "Player.hpp"
+
+
---
+++ Timeline/Classes/Player.hpp
@@ -0,0 +1,30 @@
+//
+//  Player.hpp
+//  Timeline
+//
+//  Created by Gregory Rudolph on 11/29/17.
+//  Copyright © 2017 Gregory Rudolph. All rights reserved.
+//
+
+#ifndef Player_hpp
+#define Player_hpp
+
+class Player {
+public:
+    Player();
+    
+    
+    int money;
+    // Player [X, Y]
+    int position[2];
+    
+private:
+
+    int resources[4];
+    
+    
+ 
+};
+
+
+#endif /* Player_hpp */
