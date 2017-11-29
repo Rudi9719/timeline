9064f3af378505333e277d9918053121daa44d54
xxaxun1t <hockeyfan2@live.com>
2017-11-29 16:34:06 -0500

    Updated

--- Timeline/Classes/Player.hpp
+++ Timeline/Classes/Player.hpp
@@ -13,15 +13,19 @@ class Player {
 public:
     Player();
     
-    
+
     int money;
     // Player [X, Y]
     int position[2];
-    
+    void setPosition(int x, int y)
+	int getX();
+	int getY();
 private:
 
     int resources[4];
-    
+    //These are the player positions. You probably already have them but they should be set to the default start card
+	//int x = 
+	//int y = 
     
  
 };
